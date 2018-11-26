ade6d904ea5ecb766cec1920f46157995225bd5c
wwl170131 <349478668@qq.com>
2018-11-26 21:27:11 +0800

    配置项目必要设置

--- managment/src/main.js
+++ managment/src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入插件
+import ElementUI from 'element-ui'
 
 Vue.config.productionTip = false //项目生产环境
+// 注册ElementUI
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
