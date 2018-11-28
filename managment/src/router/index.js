d927d4ea4a6a3c30c78bb2e3445a477235d58257
wwl170131 <349478668@qq.com>
2018-11-28 08:38:48 +0800

    修改home标识

--- managment/src/router/index.js
+++ managment/src/router/index.js
@@ -1,21 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../components/login/login.vue'
-import  Home from '../components/home/home.vue'
+import Home from '../components/home/home.vue'
+import User from '../components/users/users.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      name:'login',
+  routes: [{
+      name: 'login',
       path: '/login',
-      component:Login
+      component: Login
     },
     {
-      name:'home',
-      path: '/home',
+      name: 'home',
+      path: '/',
       component: Home,
+      childrens: [{
+        name: 'user',
+        path: '/user',
+        component: User
+      }]
     }
   ]
 })
